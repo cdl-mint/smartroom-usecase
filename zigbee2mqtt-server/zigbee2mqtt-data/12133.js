b4fb97f1dda8296f765fb6ad9e0611baabb5875e
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2022-08-16 09:27:37 +0200

    Update 12133.js

--- zigbee2mqtt-server/zigbee2mqtt-data/12133.js
+++ zigbee2mqtt-server/zigbee2mqtt-data/12133.js
@@ -1,1 +1,22 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const e = exposes.presets;
 
+const definition = {
+    zigbeeModel: ['TS011F'],
+    model: '12133',
+    vendor: 'Lupus',
+    description: 'Power Plug',
+    fromZigbee: [fz.on_off],
+    toZigbee: [tz.on_off],
+    exposes: [e.switch()],
+
+    // The configure method below is needed to make the device reports on/off state changes
+    // when the device is controlled manually through the button on it.
+    configure: async (device, coordinatorEndpoint, logger) => {
+        const endpoint = device.getEndpoint(1);
+        await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+    },
+};
